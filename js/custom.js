af3573c8f4dd58d580c1e442e51c0b0c46b38bf8
Sergey Butyanov <igor@tomatstudio.ru>
2020-07-10 15:04:33 +0300

    Добавление в мобильное меню телефона и кнопки закрыть

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 const menuBtn = document.querySelector('.menu-btn');
 
 menuBtn.addEventListener('click', function(){
-    let menu = document.querySelector('.menu');
+    let menu = document.querySelector('.menu-wrapper');
     this.classList.toggle('active');
     menu.classList.toggle('active');
     // document.body.classList.toggle('bg');
