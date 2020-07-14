b73cef340b0302cdb6ed63137c0d29fd7109d975
Sergey Butyanov <igor@tomatstudio.ru>
2020-07-14 16:32:39 +0300

    start Team Section and Catalog Page

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,25 @@
-const menuBtn = document.querySelector('.menu-btn');
+// const menuBtn = document.querySelector('.menu-btn');
 
-menuBtn.addEventListener('click', function(){
-    let menu = document.querySelector('.menu-wrapper');
-    this.classList.toggle('active');
-    menu.classList.toggle('active');
-    // document.body.classList.toggle('bg');
-});
+// menuBtn.addEventListener('click', function(){
+//     let menu = document.querySelector('.menu-wrapper');
+//     this.classList.toggle('active');
+//     menu.classList.toggle('active');
+//     // document.body.classList.toggle('bg');
+// });
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
